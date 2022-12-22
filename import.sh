9924994fc3a90317eae70870e2c96a9013acadac
sentientmachin3 <davideb1912@gmail.com>
2022-12-22 20:18:26 +0100

    Update background

--- import.sh
+++ import.sh
@@ -8,7 +8,4 @@ cp -R ~/.config/dunst ./
 cp ~/.zshrc ./zshrc
 cp ~/.aliases ./aliases
 cp ~/.zprofile ./zprofile
-
-mkdir -p nvim/lua
-cp ~/.config/nvim/init.vim ./nvim/init.vim
-cp ~/.config/nvim/lua/plugins.lua ./nvim/lua/plugins.lua
+cp -r ~/.config/nvim .
