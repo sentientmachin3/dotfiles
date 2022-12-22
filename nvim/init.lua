45412c86e07c9fb364785cf926fb216ac72215e2
sentientmachin3 <davideb1912@gmail.com>
2022-12-22 20:14:49 +0100

    Add nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,13 @@
+require('plugins')
+require('remaps')
+
+vim.g.lightline = { colorscheme = "carbonfox" }
+vim.cmd("colorscheme carbonfox")
+vim.opt.nu = true
+vim.opt.relativenumber = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+vim.opt.wrap = false
