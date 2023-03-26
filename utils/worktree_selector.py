172da956d531ac94a80b81b39d1a610cd356a6dc
sentientmachin3 <davideb1912@gmail.com>
2023-03-26 22:25:15 +0200

    Make worktree_selector to sessionize tmux

--- utils/worktree_selector.py
+++ utils/worktree_selector.py
@@ -2,6 +2,20 @@
 
 from pathlib import Path
 from pyfzf.pyfzf import FzfPrompt
+import argparse
+import subprocess
+
+arg_parser = argparse.ArgumentParser(
+    "worktree_selector.py", "Open a new tmux session or update the current one"
+)
+arg_parser.add_argument(
+    "-s",
+    "--sessionize",
+    help="Force opening a new session",
+    dest="sessionize",
+    action="store_true",
+)
+args = arg_parser.parse_args()
 
 fzf = FzfPrompt()
 repos_path = Path.home() / "repos"
@@ -18,11 +32,28 @@ for wt in repos_path.glob("./*/worktrees/*"):
     labels[label_key] = index
     index += 1
 
-selected = fzf.prompt(labels.keys(), "--cycle")[0]
+
+selected_list = fzf.prompt(labels.keys(), "--cycle")
+if len(selected_list) == 0:
+    exit()
+selected = selected_list[0]
 selected_index = labels[selected]
 full_worktree_path = Path(
     repos_path
     / worktrees[selected_index]["repo"]
     / worktrees[selected_index]["worktree"]
 )
-print(full_worktree_path.as_posix())
+
+if args.sessionize:
+    subprocess.check_call(
+        [
+            "tmux",
+            "neww",
+            "-n",
+            worktrees[selected_index]["worktree"],
+            "-c",
+            full_worktree_path.as_posix(),
+        ]
+    )
+else:
+    print(full_worktree_path.as_posix())
