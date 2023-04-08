4e2c5ee46c97877e5e1a98ed66487d7e9eefbc64
sentientmachin3 <davideb1912@gmail.com>
2023-04-08 15:27:23 +0200

    Remap visual lines moving

--- nvim/lua/remaps.lua
+++ nvim/lua/remaps.lua
@@ -1,8 +1,8 @@
 -- Common remaps
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
-vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
