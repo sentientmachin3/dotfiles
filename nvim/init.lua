aed10a49c0fad1ffff11238639e389cf23ff3caa
sentientmachin3 <davideb1912@gmail.com>
2023-05-06 16:49:03 +0200

    Replace netrw with nvim-tree

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,3 +1,7 @@
+-- Disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 require("configs")
 require("remaps")
 if vim.g.vscode then
--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,91 +1,105 @@
 return require("packer").startup(function(use)
-    use({ "sentientmachin3/bufswitch.nvim", requires = { "MunifTanjim/nui.nvim" } })
-    use("wbthomason/packer.nvim")
-    use("nvim-tree/nvim-web-devicons")
-    use("nvim-treesitter/nvim-treesitter")
-    use("romgrk/barbar.nvim")
-    use("nvim-lualine/lualine.nvim")
-    use("windwp/nvim-autopairs")
-    use("windwp/nvim-ts-autotag")
-    use("lewis6991/gitsigns.nvim")
-    use("numToStr/Comment.nvim")
-    use({
-        "folke/trouble.nvim",
-        requires = "nvim-tree/nvim-web-devicons",
-    })
-    use("sainnhe/gruvbox-material")
-    use({
-        "nvim-telescope/telescope.nvim",
-        requires = {
-            { "nvim-lua/plenary.nvim" },
-        },
-    })
+	use("wbthomason/packer.nvim")
+	use("nvim-treesitter/nvim-treesitter")
+	use("romgrk/barbar.nvim")
+	use("nvim-lualine/lualine.nvim")
+	use("windwp/nvim-autopairs")
+	use("windwp/nvim-ts-autotag")
+	use("lewis6991/gitsigns.nvim")
+	use("numToStr/Comment.nvim")
+	use({
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+	})
+	use("sainnhe/gruvbox-material")
+	use({
+		"nvim-telescope/telescope.nvim",
+		requires = {
+			{ "nvim-lua/plenary.nvim" },
+		},
+	})
+	use({
+		"nvim-tree/nvim-tree.lua",
+		requires = "nvim-tree/nvim-web-devicons",
+	})
 
-    -- Lsp stuff (loaded in lsp_config)
-    use("williamboman/mason.nvim")
-    use("williamboman/mason-lspconfig.nvim")
-    use("neovim/nvim-lspconfig")
-    use("jose-elias-alvarez/null-ls.nvim")
-    use("onsails/lspkind.nvim")
-    use("MunifTanjim/prettier.nvim")
-    use({
-        "hrsh7th/nvim-cmp",
-        requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
-    })
-    use({
-        "glepnir/lspsaga.nvim",
-        branch = "main",
-        requires = { { "nvim-tree/nvim-web-devicons" } },
-    })
+	-- Lsp stuff (loaded in lsp_config)
+	use("williamboman/mason.nvim")
+	use("williamboman/mason-lspconfig.nvim")
+	use("neovim/nvim-lspconfig")
+	use("jose-elias-alvarez/null-ls.nvim")
+	use("onsails/lspkind.nvim")
+	use("MunifTanjim/prettier.nvim")
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = { "hrsh7th/cmp-nvim-lsp", "L3MON4D3/LuaSnip", "saadparwaiz1/cmp_luasnip" },
+	})
+	use({
+		"glepnir/lspsaga.nvim",
+		branch = "main",
+		requires = { { "nvim-tree/nvim-web-devicons" } },
+	})
 
-    -- Plugins config
-    vim.cmd("colorscheme gruvbox-material")
-    require("bufswitch").setup()
-    require("nvim-treesitter.configs").setup({
-        ensure_installed = { "python", "typescript", "json", "rust", "lua", "go" },
-        sync_install = false,
-        auto_install = true,
-        highlight = { enable = true },
-        autotag = { enable = true },
-    })
+	-- Plugins config
+	vim.cmd("colorscheme gruvbox-material")
+	require("nvim-tree").setup({
+		view = {
+			width = vim.go.columns,
+		},
+		actions = {
+			open_file = {
+				quit_on_open = true,
+			},
+		},
+		filters = {
+			dotfiles = true,
+		},
+	})
+	require("nvim-treesitter.configs").setup({
+		ensure_installed = { "python", "typescript", "json", "rust", "lua", "go" },
+		sync_install = false,
+		auto_install = true,
+		highlight = { enable = true },
+		autotag = { enable = true },
+	})
 
-    require("lualine").setup({
-        options = {
-            icons_enabled = true,
-            theme = "gruvbox-material",
-        },
-    })
+	require("lualine").setup({
+		options = {
+			icons_enabled = true,
+			theme = "gruvbox-material",
+		},
+	})
 
-    require("bufferline").setup({
-        animation = false,
-        auto_hide = true,
-        tabpages = true,
-        clickable = true,
-        highlight_visible = true,
-        icons = {
-            diagnostics = {
-                [vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
-                [vim.diagnostic.severity.WARN] = { enabled = false },
-                [vim.diagnostic.severity.INFO] = { enabled = false },
-                [vim.diagnostic.severity.HINT] = { enabled = true },
-            },
-            buffer_index = true,
-            filetype = { enabled = true },
-        },
-        semantic_letters = false,
-        letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
-    })
+	require("bufferline").setup({
+		animation = false,
+		auto_hide = true,
+		tabpages = true,
+		clickable = true,
+		highlight_visible = true,
+		icons = {
+			diagnostics = {
+				[vim.diagnostic.severity.ERROR] = { enabled = true, icon = "ﬀ" },
+				[vim.diagnostic.severity.WARN] = { enabled = false },
+				[vim.diagnostic.severity.INFO] = { enabled = false },
+				[vim.diagnostic.severity.HINT] = { enabled = true },
+			},
+			buffer_index = true,
+			filetype = { enabled = true },
+		},
+		semantic_letters = false,
+		letters = "asdfjkl;ghnmxcvbziowerutyqpASDFJKLGHNMXCVBZIOWERUTYQP",
+	})
 
-    require("gitsigns").setup()
-    require("telescope").setup()
-    require("Comment").setup()
-    require("nvim-web-devicons").setup()
-    require("lspkind").init({
-        mode = "symbol_text",
-        preset = "codicons",
-    })
-    require("nvim-autopairs").setup()
-    require("lspsaga").setup({
-        lightbulb = { enable = false },
-    })
+	require("gitsigns").setup()
+	require("telescope").setup()
+	require("Comment").setup()
+	require("nvim-web-devicons").setup()
+	require("lspkind").init({
+		mode = "symbol_text",
+		preset = "codicons",
+	})
+	require("nvim-autopairs").setup()
+	require("lspsaga").setup({
+		lightbulb = { enable = false },
+	})
 end)
