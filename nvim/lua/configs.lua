e66441f70c6d91f428a0e9d20ed09040b0835983
sentientmachin3 <davideb1912@gmail.com>
2023-05-20 13:51:26 +0200

    Fix Ctrl-f shorcut for tmux

--- nvim/lua/configs.lua
+++ nvim/lua/configs.lua
@@ -7,11 +7,3 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 	group = highlight_group,
 	pattern = "*",
 })
-
--- Moder Lua auto-command for launching netrwPlugin which shipped with the nvim
-local mygroup = vim.api.nvim_create_augroup("loading_netrwPlugin", { clear = true })
-vim.api.nvim_create_autocmd({ "VimEnter" }, {
-	pattern = { "*" },
-	command = ":silent! Explore",
-	group = mygroup,
-})
--- nvim/lua/lsp_config.lua
+++ nvim/lua/lsp_config.lua
@@ -10,10 +10,15 @@ local on_attach = function(_, bufnr)
 	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
 	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "gd",  "<cmd>Glance definitions<CR>", bufopts)
+	vim.keymap.set("n", "gi", "<cmd>Glance implementations<CR>", bufopts)
+	vim.keymap.set("n", "gr", "<cmd>Glance references<CR>", bufopts)
+	vim.keymap.set("n", "gD", "<cmd>Glance declaration<CR>", bufopts)
+
+	-- vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	-- vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	-- vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
 	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
 
 	vim.keymap.set({ "n", "i" }, "<C-h>", vim.lsp.buf.signature_help, bufopts)
