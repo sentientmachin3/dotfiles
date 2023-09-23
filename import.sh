bf30215a16c4fbeb736fe00423711230e079f268
sentientmachin3 <davideb1912@gmail.com>
2023-09-23 13:14:36 +0200

    Remove zellij

--- import.sh
+++ import.sh
@@ -5,7 +5,6 @@ cp -R ~/.config/waybar ./
 cp -R ~/.config/wofi ./
 cp -R ~/.config/kitty ./
 cp -R ~/.config/dunst ./
-cp -R ~/.config/zellij ./
 cp ~/.zshrc ./zshrc
 cp ~/.aliases ./aliases
 cp ~/.zprofile ./zprofile
