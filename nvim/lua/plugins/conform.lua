39d7d020c82ab8bb0f4003140f5cd60f553289ae
sentientmachin3 <davideb1912@gmail.com>
2023-11-04 10:39:55 +0100

    Remove waybar persistent workspaces

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -5,11 +5,12 @@ return {
 			formatters_by_ft = {
 				lua = { "stylua" },
 				python = { "isort", "black" },
-				javascript = { { "prettierd" } },
-				typescript = { { "prettierd" } },
-				html = { { "prettierd" } },
-				typescriptreact = { { "prettierd" } },
-				go = { { "gofumpt" } },
+				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				html = { "prettierd" },
+				css = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				go = { "gofumpt" },
 			},
 		})
 	end,
