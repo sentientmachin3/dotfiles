c9417b0467a5525fccd1601e7c70d232d4f626bc
sentientmachin3 <davideb1912@gmail.com>
2024-05-12 12:20:02 +0200

    Minor tweaks

--- .config/nvim/lua/plugins/bufswitch.lua
+++
@@ -1,7 +0,0 @@
-return {
-	dir = "~/repos/bufswitch/master",
-	dependencies = { "nvim-lua/plenary.nvim" },
-	config = function()
-		require("bufswitch").setup()
-	end,
-}
--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -2,6 +2,7 @@ local wezterm = require("wezterm")
 local config = {}
 
 config.font = wezterm.font("Iosevka SS04 Medium Extended")
+--config.font = wezterm.font("FiraCode Nerd Font Mono", { weight = "Medium" })
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 config.font_size = 14.0
 config.enable_tab_bar = false
