5197693983e8164cb09ca71caa435218b32db5cf
sentientmachin3 <davideb1912@gmail.com>
2024-12-06 22:04:49 +0100

    feat: rose-pine migration

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,10 @@
 return {
-	"sainnhe/gruvbox-material",
+	"rose-pine/neovim",
+	as = "rose-pine",
 	version = false,
 	lazy = false,
 	priority = 1000,
 	config = function()
-		vim.cmd("colorscheme gruvbox-material")
+		vim.cmd("colorscheme rose-pine")
 	end,
 }
