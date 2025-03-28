1dfb3e3495effb3ac7ad356346e47fb61cee3056
sentientmachin3 <davideb1912@gmail.com>
2025-03-28 11:18:15 +0100

    remove vscode mode config

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,19 +1,17 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 
 require("remaps")
 require("ui")
-if not vim.g.vscode then
-	require("opts")
-	require("lazy").setup({ spec = "plugins", change_detection = { notify = false } })
-end
+require("opts")
+require("lazy").setup({ spec = "plugins", change_detection = { notify = false } })
