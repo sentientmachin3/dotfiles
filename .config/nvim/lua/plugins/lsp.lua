cf2009b39b7b2e75f95a34bb8d752e7a912bdfb1
sentientmachin3 <davideb1912@gmail.com>
2025-03-30 17:19:22 +0200

    start migration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,50 +1,3 @@
-local function on_lsp_attach(_, bufnr)
-    local bufopts = { noremap = true, silent = true, buffer = bufnr }
-    vim.keymap.set("n", "<leader>s", "<cmd>OrganizeImports<cr>")
-    vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>")
-    vim.keymap.set("n", "<leader>c", "<cmd>lua vim.lsp.buf.code_action()<cr>", { buffer = true })
-    vim.keymap.set("n", "<leader>f", function()
-        require("conform").format({ lsp_fallback = true })
-    end)
-    vim.keymap.set("n", "<leader>r", "<cmd>lua vim.lsp.buf.rename()<cr>")
-    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-    vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-    vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-    vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, bufopts)
-    vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, bufopts)
-    vim.keymap.set("n", "]d", vim.diagnostic.goto_next, bufopts)
-end
-
-local function setup_cmp()
-    local cmp = require("cmp")
-    cmp.setup({
-        snippet = {
-            expand = function(args)
-                require("luasnip").lsp_expand(args.body)
-            end,
-        },
-        window = {
-            documentation = cmp.config.window.bordered(),
-            completion = cmp.config.window.bordered(),
-        },
-        mapping = {
-            ["<C-p>"] = cmp.mapping.select_prev_item(),
-            ["<C-n>"] = cmp.mapping.select_next_item(),
-            ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-            ["<C-f>"] = cmp.mapping.scroll_docs(4),
-            ["<CR>"] = cmp.mapping.confirm({ select = false }),
-            ["<C-Space>"] = cmp.mapping.complete(),
-        },
-        sources = cmp.config.sources({
-            { name = "nvim_lsp" },
-        }, {
-            { name = "buffer" },
-        }),
-    })
-end
-
 local function setup_formatters()
     require("conform").setup({
         formatters_by_ft = {
@@ -76,66 +29,12 @@ local function setup_linters()
     })
 end
 
-local function get_capabilities()
-    return vim.tbl_extend(
-        "force",
-        {},
-        vim.lsp.protocol.make_client_capabilities(),
-        require("cmp_nvim_lsp").default_capabilities()
-    )
-end
-
-local function default_handler(server_name)
-    require("lspconfig")[server_name].setup({
-        on_attach = on_lsp_attach,
-        capabilities = get_capabilities(),
-    })
-end
-
-local handlers = {
-    default_handler,
-    ["lua_ls"] = function()
-        require("lspconfig").lua_ls.setup({
-            on_attach = on_lsp_attach,
-            capabilities = get_capabilities(),
-            settings = {
-                Lua = {
-                    diagnostics = {
-                        globals = { "vim" },
-                    },
-                },
-            },
-        })
-    end,
-    ["ts_ls"] = function()
-        local function organize_imports()
-            local params = {
-                command = "_typescript.organizeImports",
-                arguments = { vim.api.nvim_buf_get_name(0) },
-                title = "",
-            }
-            vim.lsp.buf.execute_command(params)
-        end
-        require("lspconfig")["ts_ls"].setup({
-            on_attach = on_lsp_attach,
-            capabilities = get_capabilities(),
-            commands = {
-                OrganizeImports = {
-                    organize_imports,
-                    description = "Organize Imports",
-                },
-            },
-        })
-    end,
-}
-
 local dependencies = {
     "nvim-tree/nvim-web-devicons",
     "nvim-lua/plenary.nvim",
-    "williamboman/mason.nvim",
     "williamboman/mason-lspconfig.nvim",
-    "hrsh7th/nvim-cmp",
-    "hrsh7th/cmp-nvim-lsp",
+    -- "hrsh7th/nvim-cmp",
+    -- "hrsh7th/cmp-nvim-lsp",
     "stevearc/conform.nvim",
     "nvimtools/none-ls-extras.nvim",
     "nvimtools/none-ls.nvim",
@@ -145,28 +44,12 @@ local dependencies = {
 }
 
 return {
-    "neovim/nvim-lspconfig",
+    "williamboman/mason.nvim",
     dependencies = dependencies,
     config = function()
         require("mason").setup()
-        require("mason-lspconfig").setup({
-            ensure_installed = {
-                "lua_ls",
-            },
-            handlers = handlers,
-        })
         require("fidget").setup()
-        setup_cmp()
         setup_formatters()
         setup_linters()
-
-        -- Diagnostics
-        vim.diagnostic.config({
-            update_in_insert = true,
-            virtual_text = true,
-            float = {
-                border = "rounded",
-            },
-        })
     end,
 }
