bc9934b70ba620d04fa4ec73e5a38358b82b375a
sentientmachin3 <davideb1912@gmail.com>
2025-04-23 11:10:44 +0200

    minor update

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -5,6 +5,16 @@ return {
 			options = {
 				icons_enabled = true,
 				theme = "rose-pine",
+				section_separators = { left = "", right = "" },
+				component_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "diagnostics" },
+				lualine_c = { "filename" },
+				lualine_x = { "filetype" },
+				lualine_y = {},
+				lualine_z = { "location" },
 			},
 		})
 	end,
