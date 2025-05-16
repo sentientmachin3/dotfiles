6a5066f2fb28787ce7e2dda4012f663fdab9ac4e
sentientmachin3 <davideb1912@gmail.com>
2025-05-16 19:22:10 +0200

    update minor stuff

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -13,17 +13,11 @@ local config = {
 			"liga=0",
 			"zero", -- Changes 0 to slashed variant.
 			"cv01", -- l with symmetrical lower stroke. (ss01)
-			"cv04", -- j with curly descender
-			"cv10", -- r with more open construction (ss01)
-			"cv11", -- y with different ascender construction (ss01)
 			"cv12", -- u with traditional construction (ss01)
-			"cv16", -- Q with bent tail
-			"cv17", -- f with curly ascender (ss02)
-			"cv18", -- 269 variant
 			"cv20", -- 5 old variant
 		},
 	}),
-	font_size = 13.5,
+	font_size = 14,
 }
 
 return config
