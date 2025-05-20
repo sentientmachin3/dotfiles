913c4f61d8bbf676275f512a15353c56ae24dbe2
sentientmachin3 <davideb1912@gmail.com>
2025-05-20 12:11:49 +0200

    update plugins and lsp configuration

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,47 +1,18 @@
-local function on_lsp_attach(_, bufnr)
-	local bufopts = { noremap = true, silent = true, buffer = bufnr }
-	vim.keymap.set("n", "<leader>s", "<cmd>OrganizeImports<cr>")
-	vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>")
-	vim.keymap.set("n", "<leader>c", "<cmd>lua vim.lsp.buf.code_action()<cr>", { buffer = true })
-	vim.keymap.set("n", "<leader>f", function()
-		require("conform").format({ lsp_fallback = true })
-	end)
-	vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
-	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
-	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
-	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
-	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
-	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
-	vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, bufopts)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, bufopts)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, bufopts)
-end
-
 local function setup_cmp()
-	local cmp = require("cmp")
-	cmp.setup({
-		snippet = {
-			expand = function(args)
-				require("luasnip").lsp_expand(args.body)
-			end,
+	require("blink.cmp").setup({
+		cmdline = { enabled = false },
+		completion = {
+			keyword = { range = "full" },
+			accept = { auto_brackets = { enabled = false } },
+			menu = {
+				auto_show = false,
+			},
+			documentation = { auto_show = true },
 		},
-		window = {
-			documentation = cmp.config.window.bordered(),
-			completion = cmp.config.window.bordered(),
+		sources = {
+			default = { "lsp", "path" },
 		},
-		mapping = {
-			["<C-p>"] = cmp.mapping.select_prev_item(),
-			["<C-n>"] = cmp.mapping.select_next_item(),
-			["<C-b>"] = cmp.mapping.scroll_docs(-4),
-			["<C-f>"] = cmp.mapping.scroll_docs(4),
-			["<CR>"] = cmp.mapping.confirm({ select = false }),
-			["<C-Space>"] = cmp.mapping.complete(),
-		},
-		sources = cmp.config.sources({
-			{ name = "nvim_lsp" },
-		}, {
-			{ name = "buffer" },
-		}),
+		signature = { enabled = true },
 	})
 end
 
@@ -79,100 +50,74 @@ local function setup_linters()
 	})
 end
 
-local function get_capabilities()
-	return vim.tbl_extend(
-		"force",
-		{},
-		vim.lsp.protocol.make_client_capabilities(),
-		require("cmp_nvim_lsp").default_capabilities()
-	)
-end
-
-local function ts_organize_imports()
-	local params = {
-		command = "_typescript.organizeImports",
-		arguments = { vim.api.nvim_buf_get_name(0) },
-		title = "",
-	}
-	vim.lsp.buf.execute_command(params)
-end
+-- local function ts_organize_imports()
+-- 	local params = {
+-- 		command = "_typescript.organizeImports",
+-- 		arguments = { vim.api.nvim_buf_get_name(0) },
+-- 		title = "",
+-- 	}
+-- 	vim.lsp.buf.execute_command(params)
+-- end
 
 local function setup_lsps()
-	local lspconfig = require("lspconfig")
-	local autosetup_lsps = {
-		"gopls",
-		"clangd",
+	local cmp = require("blink.cmp")
+	vim.lsp.config("*", {
+		capabilities = cmp.get_lsp_capabilities(),
+	})
+	vim.lsp.enable({
+		"ts_ls",
+		"lua_ls",
 		"pyright",
-		"jsonls",
-		"html",
-		"arduino_language_server",
-		"tailwindcss",
-	}
-	lspconfig.lua_ls.setup({
-		on_attach = on_lsp_attach,
-		capabilities = get_capabilities(),
-		settings = {
-			Lua = {
-				diagnostics = {
-					globals = { "vim" },
-				},
-			},
-		},
+		"json-lsp",
+		"html-lsp",
+		"clangd",
+		"docker_compose",
+		"dockerls",
+		"gopls",
+		"yamlls",
+		"tailwindcss-language-server",
 	})
-	lspconfig.ts_ls.setup({
-		on_attach = on_lsp_attach,
-		capabilities = get_capabilities(),
-		commands = {
-			OrganizeImports = {
-				ts_organize_imports,
-				description = "Organize Imports",
-			},
-		},
-	})
-	-- LSPs with an automatic setup
-	for _, lsp in ipairs(autosetup_lsps) do
-		lspconfig[lsp].setup({
-			on_attach = on_lsp_attach,
-			capabilities = get_capabilities(),
-		})
-	end
 end
 
-local dependencies = {
-	"neovim/nvim-lspconfig",
-	"nvim-tree/nvim-web-devicons",
-	"nvim-lua/plenary.nvim",
-	"williamboman/mason.nvim",
-	"hrsh7th/nvim-cmp",
-	"hrsh7th/cmp-nvim-lsp",
-	"stevearc/conform.nvim",
-	"mfussenegger/nvim-lint",
-	"j-hui/fidget.nvim",
-	"folke/trouble.nvim",
-}
-
 return {
-	"williamboman/mason-lspconfig.nvim",
-	dependencies = dependencies,
+	"neovim/nvim-lspconfig",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"nvim-lua/plenary.nvim",
+        { "saghen/blink.cmp", version = "1.*" },
+		"stevearc/conform.nvim",
+		"mfussenegger/nvim-lint",
+		"j-hui/fidget.nvim",
+		"folke/trouble.nvim",
+	},
 	config = function()
 		setup_lsps()
 		require("mason").setup()
-		require("mason-lspconfig").setup({
-			ensure_installed = {
-				"lua_ls",
-				"ts_ls",
-				"gopls",
-				"clangd",
-				"pyright",
-				"jsonls",
-				"html",
-			},
-		})
-		setup_cmp()
 		setup_formatters()
 		setup_linters()
+		setup_cmp()
 		require("fidget").setup()
-
+		vim.api.nvim_create_autocmd("LspAttach", {
+			callback = function()
+				local bufopts = { noremap = true, silent = true, buffer = bufnr }
+				vim.keymap.set("n", "<leader>s", "<cmd>OrganizeImports<cr>")
+				vim.keymap.set("n", "<leader>d", ":lua vim.diagnostic.open_float()<CR>")
+				vim.keymap.set("n", "<leader>c", "<cmd>lua vim.lsp.buf.code_action()<cr>", { buffer = true })
+				vim.keymap.set("n", "<leader>f", function()
+					require("conform").format({ lsp_fallback = true })
+				end)
+				vim.keymap.set("n", "<leader>r", vim.lsp.buf.rename, bufopts)
+				vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+				vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+				vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+				vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+				vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+				vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, bufopts)
+				vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, bufopts)
+				vim.keymap.set("n", "]d", vim.diagnostic.goto_next, bufopts)
+			end,
+		})
 		-- Diagnostics
 		vim.diagnostic.config({
 			update_in_insert = true,
