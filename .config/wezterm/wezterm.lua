9af0996cfb9603bc73ad34a5dfb3e9169671e14b
sentientmachin3 <davideb1912@gmail.com>
2025-05-25 10:40:38 +0200

    add chunk-highlight plugin

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -9,17 +9,8 @@ local config = {
 		-- family = "SFMono Nerd Font Mono",
 		family = "Berkeley Mono Medium",
 		weight = "Medium",
-		harfbuzz_features = {
-			"calt=0",
-			"clig=0",
-			"liga=0",
-			"zero", -- Changes 0 to slashed variant.
-			"cv01", -- l with symmetrical lower stroke. (ss01)
-			"cv12", -- u with traditional construction (ss01)
-			"cv20", -- 5 old variant
-		},
 	}),
-	font_size = 14
+	font_size = 15
 }
 
 return config
