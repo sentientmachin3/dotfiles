14f683892b4b3d959d823145ed1799d5bde9f21c
sentientmachin3 <davideb1912@gmail.com>
2025-09-04 20:39:30 +0200

    highlight on yank

--- .config/nvim/lua/remaps.lua
+++ .config/nvim/lua/remaps.lua
@@ -2,13 +2,6 @@
 vim.keymap.set("v", "<C-j>", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "<C-k>", ":m '<-2<CR>gv=gv")
 
--- Moving through windows
-vim.keymap.set("n", "<C-j>", "<C-w>j")
-vim.keymap.set("n", "<C-k>", "<C-w>k")
-vim.keymap.set("n", "<C-l>", "<C-w>l")
-vim.keymap.set("n", "<C-h>", "<C-w>h")
-vim.keymap.set("n", "<C-w>", "<C-w>o", { nowait = true })
-
 -- Center after search page up/down
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
 vim.keymap.set("n", "<C-u>", "<C-u>zz")
