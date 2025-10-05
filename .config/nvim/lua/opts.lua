51ec664f1c0a699c82309a1517294d053ac7d134
sentientmachin3 <davideb1912@gmail.com>
2025-10-05 15:01:35 +0200

    remove unused plugins

--- .config/nvim/lua/opts.lua
+++ .config/nvim/lua/opts.lua
@@ -26,9 +26,3 @@ vim.opt.swapfile = false
 vim.opt.eol = true
 vim.o.winborder = "rounded"
 
--- Statusline config
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "#1C2225" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#1C2225" })
-vim.api.nvim_set_hl(0, "StatusLineFilename", { fg = "#F8F9E8", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineRight", { fg = "#F8F9E8" })
-vim.opt.statusline = "%#StatusLineFilename#%t%=%#StatusLineRight#%{expand('%:e')} %l/%L:%c"
--- .config/nvim/lua/ui.lua
+++ .config/nvim/lua/ui.lua
@@ -4,3 +4,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.highlight.on_yank()
 	end,
 })
+
+-- Statusline config
+vim.api.nvim_set_hl(0, "StatusLine", { bg = "#1C2225" })
+vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#1C2225" })
+vim.api.nvim_set_hl(0, "StatusLineFilename", { fg = "#F8F9E8", bold = true })
+vim.api.nvim_set_hl(0, "StatusLineRight", { fg = "#F8F9E8" })
+vim.opt.statusline = "%#StatusLineFilename#%t%=%#StatusLineRight#%{expand('%:e')} %l/%L:%c"
