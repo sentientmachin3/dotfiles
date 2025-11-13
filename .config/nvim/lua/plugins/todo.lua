bfe6bbf59e79df2839ecf0ad85cc2d82f31c6df5
sentientmachin3 <davideb1912@gmail.com>
2025-11-13 19:09:28 +0100

    fix todos signs, remove LS_COLORS

--- .config/nvim/lua/plugins/todo.lua
+++ .config/nvim/lua/plugins/todo.lua
@@ -2,6 +2,19 @@ return {
 	"folke/todo-comments.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		require("todo-comments").setup()
+		require("todo-comments").setup({
+			keywords = {
+				FIX = {
+					icon = " ",
+					color = "error",
+					alt = { "FIXME", "BUG", "FIXIT", "ISSUE" },
+				},
+				TODO = { icon = " ", color = "info" },
+				HACK = { icon = " ", color = "warning" },
+				WARN = { icon = " ", color = "warning", alt = { "WARNING" } },
+				PERF = { icon = " " },
+				NOTE = { icon = " ", color = "hint", alt = { "INFO" } },
+			},
+		})
 	end,
 }
