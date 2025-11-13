561a5db68d73b86acc473e36b11df788b7b00c51
sentientmachin3 <davideb1912@gmail.com>
2025-11-13 19:12:24 +0100

    cycle over branches when selecting one

--- .local/bin/checkout-remote-branch.sh
+++ .local/bin/checkout-remote-branch.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
 
-chosen_branch_name=$(git branch --remote | sed "s|origin/||g" | fzf | sed "s/ //g")
+chosen_branch_name=$(git branch --remote | sed "s|origin/||g" | fzf --cycle | sed "s/ //g")
 worktree_dir_name=$(echo $chosen_branch_name | sed 's|^[^/]*/||')
 git worktree add $worktree_dir_name $chosen_branch_name
