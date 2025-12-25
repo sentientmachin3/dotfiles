5b319dcaeef2af2b5a3f5b0f2b5e7a1690cdd187
sentientmachin3 <davideb1912@gmail.com>
2025-12-25 18:07:42 +0100

    minor updates

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -2,15 +2,13 @@ return {
 	"nvim-telescope/telescope.nvim",
 	dependencies = { "nvim-lua/plenary.nvim" },
 	config = function()
-		local actions = require("telescope.actions")
 		require("telescope").setup({
 			defaults = {
 				mappings = {
-					n = {
-						["<C-w>"] = actions.delete_buffer,
-					},
 					i = {
-						["<C-w>"] = actions.delete_buffer,
+						["<C-w>"] = function()
+							vim.cmd([[normal! bcw]])
+						end,
 					},
 				},
 			},
@@ -19,6 +17,5 @@ return {
 		vim.keymap.set("n", "<leader>af", builtins.git_files, {})
 		vim.keymap.set("n", "<leader>sf", builtins.find_files, {})
 		vim.keymap.set("n", "<leader>g", builtins.live_grep, {})
-		vim.keymap.set("n", "<C-b>", builtins.buffers, {})
 	end,
 }
