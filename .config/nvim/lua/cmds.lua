d743fb213517592d41cafb30f40936dce713f7bd
sentientmachin3 <davideb1912@gmail.com>
2025-12-25 18:58:46 +0100

    Update statusbar content

--- .config/nvim/lua/cmds.lua
+++ .config/nvim/lua/cmds.lua
@@ -16,3 +16,8 @@ vim.api.nvim_create_autocmd("User", {
 vim.api.nvim_create_user_command("BlameLine", function()
 	require("gitsigns").blame_line()
 end, { desc = "Open current line blame in floating window" })
+
+-- Close all other buffers except the current one
+vim.api.nvim_create_user_command("CloseOthers", function()
+	vim.cmd("w | %bd | e#")
+end, { desc = "Close all other buffers" })
--- .config/nvim/lua/ui.lua
+++ .config/nvim/lua/ui.lua
@@ -6,8 +6,8 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 })
 
 -- Statusline config
-vim.api.nvim_set_hl(0, "StatusLine", { bg = "#1C2225" })
-vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#1C2225" })
-vim.api.nvim_set_hl(0, "StatusLineFilename", { fg = "#F8F9E8", bold = true })
-vim.api.nvim_set_hl(0, "StatusLineRight", { fg = "#F8F9E8" })
-vim.opt.statusline = "%#StatusLineFilename#%t%=%#StatusLineRight#%{expand('%:e')} %l/%L:%c"
+-- vim.api.nvim_set_hl(0, "StatusLine", { bg = "#1C2225" })
+-- vim.api.nvim_set_hl(0, "StatusLineNC", { bg = "#1C2225" })
+vim.api.nvim_set_hl(0, "StatusLineFilename", { fg = "#1C2225", bold = true })
+vim.api.nvim_set_hl(0, "StatusLineRight", { fg = "#1C2225" })
+vim.opt.statusline = "%#StatusLineFilename#%f%=%#StatusLineRight#%{expand('%:e')} %l/%L:%c"
