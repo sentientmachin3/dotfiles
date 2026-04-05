c9d67470c05e7cd882e1f0b2214bd9bbbfae8f85
sentientmachin3 <davideb1912@gmail.com>
2026-04-05 22:19:50 +0200

    remove telescope fix

--- .config/nvim/lua/cmds.lua
+++ .config/nvim/lua/cmds.lua
@@ -1,17 +1,3 @@
--- remove when telescope fixes the winborder bug
-vim.api.nvim_create_autocmd("User", {
-	pattern = "TelescopeFindPre",
-	callback = function()
-		vim.opt_local.winborder = "none"
-		vim.api.nvim_create_autocmd("WinLeave", {
-			once = true,
-			callback = function()
-				vim.opt_local.winborder = "rounded"
-			end,
-		})
-	end,
-})
-
 -- LineBlame of current line
 vim.api.nvim_create_user_command("BlameLine", function()
 	require("gitsigns").blame_line()
